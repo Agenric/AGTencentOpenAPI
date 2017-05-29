6281c7a0dd8fbca670b58adce0e945799510ccfe
AgenricWon <AgenricWon@gmail.com>
2017-05-29 23:12:47 +0800

    Initial commit

---
+++ Example/AGTencentOpenAPI/AGViewController.m
@@ -0,0 +1,29 @@
+//
+//  AGViewController.m
+//  AGTencentOpenAPI
+//
+//  Created by agenric on 05/29/2017.
+//  Copyright (c) 2017 agenric. All rights reserved.
+//
+
+#import "AGViewController.h"
+
+@interface AGViewController ()
+
+@end
+
+@implementation AGViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
